4696fc07a61ef7427bb4e5df005f1f761e994357
Oliver Smart <oliver.s.smart@gmail.com>
2019-05-08 12:51:29 +0100

    refactor four_of_a_kind, see GitHub issue #1 passes tests

--- python/yatzy.py
+++ python/yatzy.py
@@ -71,18 +71,12 @@ class Yatzy:
         return YatzyClean([d1, d2, d3, d4, d5]).two_pair
 
     @staticmethod
-    def four_of_a_kind( _1,  _2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[_1-1] += 1
-        tallies[_2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        for i in range(6):
-            if (tallies[i] >= 4):
-                return (i+1) * 4
-        return 0
-
+    def four_of_a_kind(d1,  d2,  d3,  d4,  d5):
+        """
+        If there are four dice with the same number,
+        the player scores the sum of these dice.
+        """
+        return YatzyClean([d1, d2, d3, d4, d5]).four_of_a_kind
 
     @staticmethod
     def three_of_a_kind( d1,  d2,  d3,  d4,  d5):
@@ -247,6 +241,19 @@ class YatzyClean(object):
                 pair.append(d)
         if len(pair) == 2:
             return 2*sum(pair)
+        else:
+            return 0
+
+    @property
+    def four_of_a_kind(self):
+        """
+        If there are four dice with the same number,
+        the player scores the sum of these dice.
+        """
+        counts = Counter(self.dice)
+        pips, count = counts.most_common(1)[0]
+        if count >= 4:
+            return 4*pips
         else:
             return 0
 
